2eeecc49ebfe874a556356985972f024a93ffd90
Patrick Miller <paddymills@proton.me>
2024-06-28 14:18:19 -0400

    add batch listing placeholder

---
+++ client/src/BatchListing.tsx
@@ -0,0 +1,15 @@
+
+
+import { Component  } from "solid-js";
+
+
+const BatchListing: Component = () => {
+
+  return (
+    <>
+      <p>Not implemented</p>
+    </>
+  );
+};
+
+export default BatchListing;
