9be5e402bd03562e086f0dd078554bfb7a5544d4
Patrick Miller <paddymills@proton.me>
2024-06-28 14:16:55 -0400

    add footer and stuff

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,25 @@
 import { type Component, createSignal, For } from "solid-js";
 
 import BatchAssign from "./BatchAssign";
+import BatchAssignNew from "./BatchAssignNew";
+import BatchListing from "./BatchListing";
 import { Dynamic } from "solid-js/web";
 
 const apps = [
+    { name: "Batch Assign (dev)",   component: BatchAssignNew },
     { name: "Batch Assign",         component: BatchAssign },
-    { name: "Batch Listing",        component: () => <p>Not implemented</p> },
-    { name: "Sigmanest director",   component: () => <p>Not implemented</p> },
+    { name: "Batch Listing",        component: BatchListing },
 ]
 const [currentApp, setCurrentApp] = createSignal(0);
 
 const App: Component = () => {
   return (
     <>
-      <header class="bg-gray-800 p-4 flex items-center space-x-2 justify-between items-center">
+      <nav class="bg-gray-800 p-4 flex items-center space-x-2 justify-between items-center">
         <div class="flex items-center space-x-2">
           <For each={apps}>{(app, i) =>
             <button
-                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+                class="bg-gradient-to-r from-cyan-500 to-blue-500 transition ease-in-out duration-300 hover:scale-110 hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 text-white font-bold py-2 px-4 rounded-full"
                 classList={{ "text-black": i() === currentApp() }}
                 onClick={() => setCurrentApp(i)}
             >
@@ -25,10 +27,14 @@ const App: Component = () => {
             </button>
             }</For>
         </div>
-      </header>
-      <div class="min-h-screen bg-gray-100 flex flex-col items-center justify-center space-y-4">
+      </nav>
+      <main class="min-h-full flex flex-col grow justify-around place-items-center">
         <Dynamic component={apps[currentApp()].component} />
-      </div>
+      </main>
+      <footer class="bg-gray-800 px-2 py-1 flex justify-between font-semibold tracking-wide select-none">
+        <p class="text-white">&copy; {new Date().getFullYear()} by High Steel Structures LLC</p>
+        <p class="text-white/50 text-xs place-self-center">made with <span class="text-red-500">&hearts;</span> by Patrick Miller</p>
+      </footer>
     </>
   );
 };
