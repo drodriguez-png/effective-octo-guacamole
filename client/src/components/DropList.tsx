7b55475b7e94c3732b2f5ccc2caa915b83d16d23
Patrick Miller <paddymills@proton.me>
2024-07-12 12:53:18 -0400

    handle default

--- client/src/components/DropList.tsx
+++ client/src/components/DropList.tsx
@@ -1,9 +1,15 @@
-import { type Component, createSignal, For } from "solid-js";
+import { type Component, createSignal, For, type Setter, createEffect } from "solid-js";
 
-type DropListProps = { class?: string, name: string, items: string[] };
+type DropListProps = {
+  class?: string,
+  name: string,
+  items: string[],
+  setValue: Setter<string>,
+  value?: string
+};
 
 const DropList: Component<DropListProps> = (props: DropListProps) => {
-  const [selected, setSelected] = createSignal(0)
+  console.log(props);
 
   return (
     <div class={`${props.class} flex flex-col`}>
@@ -12,9 +18,9 @@ const DropList: Component<DropListProps> = (props: DropListProps) => {
         class="rounded-lg px-2 focus:outline-none focus:ring border-blue-500"
         name={props.name}
         title={props.name}
-        value={props.items[selected()]}
+        value={props.value || props.items.length > 0 ? props.items[0] : ""}
         onInput={(e) =>
-          setSelected((e.target as HTMLSelectElement).selectedIndex)
+          props.setValue((e.currentTarget as HTMLSelectElement).value)
         }
       >
         <For each={props.items}>{(item) => <option value={item}>{item}</option>}</For>
