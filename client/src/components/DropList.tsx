2f5b8be8c39953ce27c7899716d437b034a6656a
Patrick Miller <paddymills@proton.me>
2024-07-12 13:22:22 -0400

    using kobalte

--- client/src/components/DropList.tsx
+++ client/src/components/DropList.tsx
@@ -1,31 +1,49 @@
-import { type Component, createSignal, For, type Setter, createEffect } from "solid-js";
+import { type Component, type Accessor, type Setter } from "solid-js";
+import { Select } from "@kobalte/core/select";
+import { FiCheck, FiChevronDown } from "solid-icons/fi";
+import "./DropList.css";
 
 type DropListProps = {
-  class?: string,
-  name: string,
-  items: string[],
-  setValue: Setter<string>,
-  value?: string
+  class?: string;
+  name: string;
+  items: string[];
+  setValue: Setter<string>;
+  value: Accessor<string>;
 };
 
 const DropList: Component<DropListProps> = (props: DropListProps) => {
   console.log(props);
 
   return (
-    <div class={`${props.class} flex flex-col`}>
-      <label class="text-sm" for={props.name}>{props.name}</label>
-      <select
-        class="rounded-lg px-2 focus:outline-none focus:ring border-blue-500"
-        name={props.name}
-        title={props.name}
-        value={props.value || props.items.length > 0 ? props.items[0] : ""}
-        onInput={(e) =>
-          props.setValue((e.currentTarget as HTMLSelectElement).value)
-        }
-      >
-        <For each={props.items}>{(item) => <option value={item}>{item}</option>}</For>
-      </select>
-    </div>
+    <Select
+      class="m-2"
+      options={props.items}
+      value={props.value()}
+      onChange={props.setValue}
+      placeholder="select an item..."
+      itemComponent={(props) => (
+        <Select.Item item={props.item} class="select__item">
+          <Select.ItemLabel>{props.item.rawValue}</Select.ItemLabel>
+          <Select.ItemIndicator class="select__item-indicator">
+            <FiCheck />
+          </Select.ItemIndicator>
+        </Select.Item>
+      )}
+    >
+      <Select.Trigger class="select__trigger" aria-label={props.name}>
+        <Select.Value<string> class="select__value">
+          {(state) => state.selectedOption()}
+        </Select.Value>
+        <Select.Icon class="select__icon">
+          <FiChevronDown />
+        </Select.Icon>
+      </Select.Trigger>
+      <Select.Portal>
+        <Select.Content class="select__content">
+          <Select.Listbox class="select__listbox" />
+        </Select.Content>
+      </Select.Portal>
+    </Select>
   );
 };
 
