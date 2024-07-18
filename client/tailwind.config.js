8e84e85ebf8290cefa5ac68a955d6d96363632ae
Patrick Miller <paddymills@proton.me>
2024-07-18 08:19:25 -0400

    format ES module

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    container: {
-      center: true,
-    },
-    extend: {},
+export const content = ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"];
+export const theme = {
+  container: {
+    center: true,
   },
-  plugins: [],
+  extend: {},
 };
+export const plugins = [];
