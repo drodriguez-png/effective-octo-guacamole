29b04a17df829427b1a7a4b6eae753c20e7a1cae
Patrick Miller <paddymills@proton.me>
2024-07-22 09:12:56 -0400

    add style

--- client/src/NotFound.tsx
+++ client/src/NotFound.tsx
@@ -3,7 +3,9 @@ import { Component } from "solid-js";
 export const NotFound: Component = () => {
   return (
     <div>
-      <h1>Whoops! This page does not exist.</h1>
+      <h1 class="font-sans text-lg font-medium">
+        whoops! this page does not exist.
+      </h1>
     </div>
   );
 };
