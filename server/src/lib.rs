3f7ecbe8b3523bf9b88e2ea3a8d018b3ea5b1751
Patrick Miller <paddymills@proton.me>
2024-07-24 15:19:50 -0400

    add thiserror

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -5,40 +5,44 @@ pub mod error {
         http::StatusCode,
         response::{IntoResponse, Response},
     };
-    use tiberius::error::Error as SqlError;
 
     // Error handling: see
     //  https://docs.rs/axum/latest/axum/error_handling/index.html
     //  https://github.com/tokio-rs/axum/blob/main/examples/anyhow-error-response/src/main.rs
-    #[derive(Debug)]
+    #[derive(Debug, thiserror::Error)]
     pub enum Error {
-        SqlError,
+        #[error("Database error: see server logs.")]
+        SqlError(#[from] tiberius::error::Error),
+        #[error("Database pool error: see server logs.")]
+        SqlPoolError,
+        #[error("Requested resource not found")]
         NotFound(String),
     }
 
     // Tell axum how to convert `AppError` into a response.
     impl IntoResponse for Error {
         fn into_response(self) -> Response {
-            let error = match self {
-                Self::SqlError => String::from("Database error: see server logs."),
-                Self::NotFound(s) => s,
-            };
+            // let error = match self {
+            //     Self::SqlError(e) => e.to_string(),
+            //     Self::SqlPoolError => todo!(),
+            //     Self::NotFound(s) => s,
+            // };
 
-            (StatusCode::INTERNAL_SERVER_ERROR, error).into_response()
+            (StatusCode::INTERNAL_SERVER_ERROR, self.to_string()).into_response()
         }
     }
 
-    impl From<SqlError> for Error {
-        fn from(value: SqlError) -> Self {
-            log::error!("Casting tiberius error to app error: {:#?}", value);
-            Self::SqlError
-        }
-    }
+    // impl From<SqlError> for Error {
+    //     fn from(value: SqlError) -> Self {
+    //         log::error!("Casting tiberius error to app error: {:#?}", value);
+    //         Self::SqlError
+    //     }
+    // }
 
     impl<T: std::fmt::Debug> From<bb8::RunError<T>> for Error {
         fn from(value: bb8::RunError<T>) -> Self {
-            log::error!("Casting tiberius error to app error: {:#?}", value);
-            Self::SqlError
+            log::error!("Casting bb8 error to app error: {:#?}", value);
+            Self::SqlPoolError
         }
     }
 }
