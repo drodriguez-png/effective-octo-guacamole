b63177642eb23079054377a6b33856e21b22173b
Patrick Miller <pimiller@high.net>
2025-01-08 11:31:02 -0500

    refactor to regex

--- schema/gen_proc.py
+++ schema/gen_proc.py
@@ -1,26 +1,45 @@
 from os import path
+import re
+
+USE_DB = re.compile("USE SNDBaseISap;")
+SIMTRANS_DB = re.compile(r"(FROM|INTO) SNDBaseDev(\.\w+\.\w+)")
+CONFIG = re.compile(
+    r"\('[A-Z]{3}', \d+, '(\\\\hssieng\\)\w+([\\a-zA-Z]+<\w+>.dxf)'([^\n]*)\)"
+)
 
 template_file = path.join(path.dirname(__file__), "SimTransCtrl_Proc.sql")
-dbs = [
-    # SAP Env, Database, District, SimTransDatabase
-    ("PRD", "SNDBasePrd", 1, "SNDBasePrd"),
-    ("QAS", "SNDBaseQas", 2, "SNDBaseDev"),
-    ("DEV", "SNDBaseDev", 3, "SNDBaseDev"),
-    ("SBX", "SNDBaseSbx", 4, "SNDBaseDev"),
-]
+dbs = {
+    # "SimTrans Database": [("Env", "Sigmanest Database", "District")],
+    "SNDBasePrd": [
+        ("PRD", "SNDBasePrd", 1),
+    ],
+    "SNDBaseDev": [
+        ("QAS", "SNDBaseQas", 2),
+        ("DEV", "SNDBaseDev", 3),
+        ("SBX", "SNDBaseSbx", 4),
+    ],
+}
 
 with open(template_file) as sql:
     template = sql.read()
 
-for env, db, district, simtransdb in dbs:
-    network_path = path.join(r"\\hssieng", db)
 
-    sql = template.replace("SNDBaseISap", db)
-    sql = sql.replace("SNDBaseDev", simtransdb)
-    sql = sql.replace(
-        r"'QAS', 1, '\\hssieng\SNDataQas",
-        "'{}', {}, '{}".format(env, district, network_path),
-    )
+def generate(env, db, district, simtransdb):
+    sql = USE_DB.sub(f"USE {db};", template)
+    sql = SIMTRANS_DB.sub(f"\\1 {simtransdb}\\2", sql)
+    sql = CONFIG.sub(f"('{env}', {district}, '\\1{db}\\2'\\3)", sql)
 
     with open("SimTransCtrl_Proc_{}.sql".format(env), "w") as sql_file:
         sql_file.write(sql)
+
+
+def main():
+    for db, env in dbs.items():
+        generate(*env, simtransdb)
+
+
+if __name__ == "__main__":
+    main()
+
+    # testing
+    # generate(*dbs["SNDBaseDev"][2], "SNDBaseDev")
