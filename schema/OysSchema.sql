4aeb9303c4deae9fd23298df05484962cf15f6ae
Patrick Miller <pimiller@high.net>
2025-01-16 10:46:15 -0500

    add OYS suggested changes

--- schema/OysSchema.sql
+++ schema/OysSchema.sql
@@ -16,39 +16,73 @@ GO
 -- ????????? ????????????
 -- ? Parts ? ? Remnants ?
 -- ????????? ????????????
-CREATE TABLE oys.Programs (
-	Id INT IDENTITY(1,1) PRIMARY KEY,
-	ArchivePacketID INT,	-- TODO: can this be removed?
+CREATE TABLE oys.Program (
+	AutoId INT IDENTITY(1,1),
+	ProgramGUID UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
 	ProgramName VARCHAR(50),
 	RepeatID INT,
 	MachineName VARCHAR(50),
-	CuttingTime FLOAT
+	CuttingTime FLOAT,
+	TaskName VARCHAR(50),
+	NestType VARCHAR(64),	-- 'Slab' or 'NormalNest'
+	Status VARCHAR(64),	-- 'Created', 'Released', or 'Deleted'
+	WSName NVARCHAR(300),
+	EntryDateTime DateTime
 );
-CREATE TABLE oys.Sheets (
-	Id INT IDENTITY(1,1) PRIMARY KEY,
-	Program INT FOREIGN KEY REFERENCES oys.Programs(Id),
-
+CREATE TABLE oys.ParentPlate(
+	AutoId INT IDENTITY(1,1),
+	ProgramGUID UNIQUEIDENTIFIER
+		FOREIGN KEY REFERENCES oys.Program(ProgramGUID) ON DELETE CASCADE,
+	ParentPlateGUID UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
+	PlateName VARCHAR(50),	-- TODO: If using Layout, do we need this?
+	Material VARCHAR(50),
+	Thickness FLOAT
+);
+CREATE TABLE oys.ParentPart(
+	AutoId INT IDENTITY(1,1) PRIMARY KEY,
+	ProgramGUID UNIQUEIDENTIFIER
+		FOREIGN KEY REFERENCES oys.Program(ProgramGUID) ON DELETE CASCADE,
+	PartName VARCHAR(100),	-- from Data3 (SAP part name), not PartName
+	PartQty INT,			-- QtyProgram
+	Job VARCHAR(50),		-- Data1, TODO: do we need this?
+	Shipment VARCHAR(50),	-- Data2, TODO: do we need this?
+	TrueArea FLOAT,			-- TODO: do we need this?
+	NestedArea FLOAT		-- TODO: do we need this?
+);
+CREATE TABLE oys.ChildPlate (
+	AutoId INT IDENTITY(1,1),
+	ProgramGUID UNIQUEIDENTIFIER
+		FOREIGN KEY REFERENCES oys.Program(ProgramGUID) ON DELETE CASCADE,
+	ChildPlateGUID UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
+	
+	PlateName VARCHAR(50),
 	-- For tracking the order of sheets in a slab
 	--	- Indexes start at 1
 	--	- Indexes represent the order of sheets Left-to-Right in a slab
-	SheetIndex INT NOT NULL DEFAULT 1,
+	PlateIndex INT NOT NULL DEFAULT 1,
 
-	SheetName VARCHAR(50),
-	MaterialMaster VARCHAR(50)	-- PrimeCode
+	MaterialMaster VARCHAR(50),	-- PrimeCode
+	Material VARCHAR(50),
+	Thickness FLOAT,
+	ChildNestTaskName VARCHAR(50),
+	ChildNestProgramName VARCHAR(50),
+	ChildNestRepeatID INT
 );
-CREATE TABLE oys.Parts (
-	Id INT IDENTITY(1,1) PRIMARY KEY,
-	Sheet INT FOREIGN KEY REFERENCES oys.Sheets(Id),
+CREATE TABLE oys.ChildPart (
+	AutoId INT IDENTITY(1,1) PRIMARY KEY,
+	ChildPlateGUID UNIQUEIDENTIFIER
+		FOREIGN KEY REFERENCES oys.ChildPlate(ChildPlateGUID) ON DELETE CASCADE,
 	PartName VARCHAR(100),	-- from Data3 (SAP part name), not PartName
-	Qty INT,				-- QtyProgram
+	PartQty INT,			-- QtyProgram
 	Job VARCHAR(50),		-- Data1
 	Shipment VARCHAR(50),	-- Data2
 	TrueArea FLOAT,
 	NestedArea FLOAT
 );
-CREATE TABLE oys.Remnants (
-	Id INT IDENTITY(1,1) PRIMARY KEY,
-	Sheet INT FOREIGN KEY REFERENCES oys.Sheets(Id),
+CREATE TABLE oys.Remnant (
+	AutoId INT IDENTITY(1,1) PRIMARY KEY,
+	ChildPlateGUID UNIQUEIDENTIFIER
+		FOREIGN KEY REFERENCES oys.ChildPlate(ChildPlateGUID) ON DELETE CASCADE,
 	RemnantName VARCHAR(50),
 	Area FLOAT,
 
@@ -59,9 +93,10 @@ CREATE TABLE oys.Remnants (
 
 -- Program state feedback
 CREATE TABLE oys.Feedback (
-	Id INT IDENTITY(1,1) PRIMARY KEY,
-	Program INT FOREIGN KEY REFERENCES oys.Programs(Id),
-	
+	AutoId INT IDENTITY(1,1) PRIMARY KEY,
+	ProgramGUID UNIQUEIDENTIFIER
+		FOREIGN KEY REFERENCES oys.Program(ProgramGUID) ON DELETE CASCADE,
+
 	-- Status of the program
 	--	- Created: program posting (SN100)
 	--	- Deleted: program delete (SN101)
