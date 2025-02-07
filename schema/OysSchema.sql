11a4d35cade0b21964b681811898b241c36e5e66
Patrick Miller <paddymills@proton.me>
2025-02-07 13:45:08 -0500

    add oys changes

--- schema/OysSchema.sql
+++ schema/OysSchema.sql
@@ -1,116 +1,176 @@
--- Purpose: OYS posting plugin schema for Interface 3
-USE SNDBaseDev;
+﻿USE master
+GO
+
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'SNDBaseDev') BEGIN
+   CREATE DATABASE [SNDBaseDev]
+END
+GO
+
+USE SNDBaseDev
 GO
 
 CREATE SCHEMA oys;
 GO
 
+USE SNDBaseDev
 CREATE TABLE oys.Program (
-	AutoId INT IDENTITY(1,1),
-	DBEntryDateTime DATETIME DEFAULT GETDATE(),
-	ProgramGUID UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
-	ProgramName VARCHAR(50),
-	MachineName VARCHAR(50),
-	CuttingTime FLOAT,
-	TaskName VARCHAR(50),
-	NestType VARCHAR(64),	-- 'Slab', 'Standard' or 'Split'
-	WSName NVARCHAR(300)
-);
-CREATE TABLE oys.ParentPlate(
-	AutoId INT IDENTITY(1,1),
-	DBEntryDateTime DATETIME DEFAULT GETDATE(),
-	ProgramGUID UNIQUEIDENTIFIER
-		FOREIGN KEY REFERENCES oys.Program(ProgramGUID) ON DELETE CASCADE,
-	ParentPlateGUID UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
-	PlateName VARCHAR(50),
-	Material VARCHAR(50),
-	Thickness FLOAT
-);
-CREATE TABLE oys.ParentPart(
-	AutoId INT IDENTITY(1,1) PRIMARY KEY,
-	DBEntryDateTime DATETIME DEFAULT GETDATE(),
-	ProgramGUID UNIQUEIDENTIFIER
-		FOREIGN KEY REFERENCES oys.Program(ProgramGUID) ON DELETE CASCADE,
-	SNPartName VARCHAR(100),
-	QtyProgram INT,
-	TrueArea FLOAT,
-	NestedArea FLOAT
-);
-CREATE TABLE oys.ChildPlate (
-	AutoId INT IDENTITY(1,1),
-	DBEntryDateTime DATETIME DEFAULT GETDATE(),
-	ProgramGUID UNIQUEIDENTIFIER
-		FOREIGN KEY REFERENCES oys.Program(ProgramGUID) ON DELETE CASCADE,
-	ChildPlateGUID UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
-
-	PlateName VARCHAR(50),
-	-- For tracking the order of sheets in a slab
-	--	- Indexes start at 1
-	--	- Indexes represent the order of sheets Left-to-Right in a slab
-	PlateIndex INT NOT NULL DEFAULT 1,
-
-	MaterialMaster VARCHAR(50),	-- PrimeCode
-	Material VARCHAR(50),
-	Thickness FLOAT,
-	ChildNestTaskName VARCHAR(50),
-	ChildNestProgramName VARCHAR(50),
-	ChildNestRepeatID INT
-);
-CREATE TABLE oys.ChildPart (
-	AutoId INT IDENTITY(1,1) PRIMARY KEY,
-	DBEntryDateTime DATETIME DEFAULT GETDATE(),
-	ChildPlateGUID UNIQUEIDENTIFIER
-		FOREIGN KEY REFERENCES oys.ChildPlate(ChildPlateGUID) ON DELETE CASCADE,
-	SNPartName VARCHAR(100),	-- PartName
-	SAPPartName VARCHAR(100),	-- from Data3 (SAP part name)
-	QtyProgram INT,
-	Job VARCHAR(50),		-- Data1
-	Shipment VARCHAR(50),	-- Data2
-	TrueArea FLOAT,
-	NestedArea FLOAT
-);
-CREATE TABLE oys.Remnant (
-	AutoId INT IDENTITY(1,1) PRIMARY KEY,
-	DBEntryDateTime DATETIME DEFAULT GETDATE(),
-	ChildPlateGUID UNIQUEIDENTIFIER
-		FOREIGN KEY REFERENCES oys.ChildPlate(ChildPlateGUID) ON DELETE CASCADE,
-	RemnantName VARCHAR(50),
-	Area FLOAT,
-
-	-- For visibility in SAP of whether or not a sheet geometry can be understood
-	--	as Length * Width or if inquiry in Sigmanest is required
-	IsRectangular BIT,
-	RectWidth FLOAT,
-	RectLength FLOAT
-);
-
--- Program state feedback
-CREATE TABLE oys.Status (
-	AutoId INT IDENTITY(1,1) PRIMARY KEY,
-	DBEntryDateTime DATETIME DEFAULT GETDATE(),
-	ProgramGUID UNIQUEIDENTIFIER
-		FOREIGN KEY REFERENCES oys.Program(ProgramGUID) ON DELETE CASCADE,
-
-	-- Status of the program
-	--	- Created: program posting (SN100)
-	--	- Deleted: program delete (SN101)
-	--	- Released: program was checked (where applicable)
-	SigmanestStatus VARCHAR(64),	-- 'Created', 'Released', or 'Deleted'
-
-	-- For tracking status of data pushed to SAP
-	--	- Initially this should be null
-	--	- Boomi and the procedures Boomi calls will update this column
-	SapStatus VARCHAR(64) NULL
-);
-CREATE TABLE oys.StatusArchive (
-	-- This table is a "dumb" copy of what was in oys.Status
-	-- All foreign keys have been removed to avoid any cascading effects
-
-	AutoId INT PRIMARY KEY,
-	DBEntryDateTime DATETIME DEFAULT GETDATE(),
-	ProgramGUID UNIQUEIDENTIFIER,	-- was a foreign key to oys.Program(ProgramGUID)
-	SigmanestStatus VARCHAR(64),
-	SapStatus VARCHAR(64) NULL,
-	ArchiveDateTime DATETIME DEFAULT GETDATE(),
+    AutoId BIGINT IDENTITY(1,1) NOT NULL,
+    DBEntryDateTime DATETIME DEFAULT GETDATE(),
+    ProgramGUID UNIQUEIDENTIFIER NOT NULL,
+    ProgramName VARCHAR(50),
+    LayoutNumber INT,
+    MachineName VARCHAR(50),
+    CuttingTime FLOAT,
+    TaskName VARCHAR(50),
+    NestType VARCHAR(64),	-- 'Slab', 'Standard' or 'Split'
+    WSName NVARCHAR(300)
 );
 GO
+ALTER TABLE oys.Program
+ADD CONSTRAINT PK_Program PRIMARY KEY NONCLUSTERED (ProgramGUID);
+CREATE UNIQUE CLUSTERED INDEX IX_AutoId ON oys.Program(AutoId);
+GO
+
+USE SNDBaseDev
+CREATE TABLE oys.ParentPlate
+(
+    AutoId BIGINT IDENTITY(1,1) NOT NULL,
+    DBEntryDateTime DATETIME DEFAULT GETDATE(),
+    ProgramGUID UNIQUEIDENTIFIER NOT NULL,
+    PlateName VARCHAR(50),
+    Material VARCHAR(50),
+    Thickness FLOAT,
+    Area FLOAT
+);
+GO
+ALTER TABLE oys.ParentPlate
+ADD CONSTRAINT PK_ParentPlate PRIMARY KEY NONCLUSTERED (ProgramGUID);
+ALTER TABLE oys.ParentPlate
+ADD CONSTRAINT FK_ParentPlate_Program FOREIGN KEY (ProgramGUID) 
+  REFERENCES oys.Program(ProgramGUID) ON DELETE CASCADE;
+CREATE UNIQUE CLUSTERED INDEX IX_AutoId ON oys.ParentPlate(AutoId);
+GO
+
+USE SNDBaseDev
+CREATE TABLE oys.ParentPart(
+    AutoId BIGINT IDENTITY(1,1) NOT NULL,
+    DBEntryDateTime DATETIME DEFAULT GETDATE(),
+    ProgramGUID UNIQUEIDENTIFIER NOT NULL,
+    ParentPartGUID UNIQUEIDENTIFIER NOT NULL,
+    SNPartName VARCHAR(100),
+    QtyProgram INT,
+    TrueArea FLOAT,
+    NestedArea FLOAT
+);
+GO
+ALTER TABLE oys.ParentPart
+ADD CONSTRAINT PK_ParentPart PRIMARY KEY NONCLUSTERED (ProgramGUID,ParentPartGUID);
+ALTER TABLE oys.ParentPart
+ADD CONSTRAINT FK_ParentPart_ParentPlate FOREIGN KEY (ProgramGUID) 
+  REFERENCES oys.ParentPlate(ProgramGUID) ON DELETE CASCADE;
+CREATE UNIQUE CLUSTERED INDEX IX_AutoId ON oys.ParentPart(AutoId);
+GO
+
+USE SNDBaseDev
+CREATE TABLE oys.ChildPlate (
+    AutoId BIGINT IDENTITY(1,1) NOT NULL,
+    DBEntryDateTime DATETIME DEFAULT GETDATE(),
+    ProgramGUID UNIQUEIDENTIFIER NOT NULL,
+    ChildPlateGUID UNIQUEIDENTIFIER NOT NULL,
+    PlateName VARCHAR(50),
+    PlateNumber INT NOT NULL DEFAULT 1,
+    MaterialMaster VARCHAR(50),
+    Material VARCHAR(50),
+    Thickness FLOAT,
+    ChildNestTaskName VARCHAR(50),
+    ChildNestProgramName VARCHAR(50),
+    ChildNestRepeatID INT,
+    Area FLOAT
+);
+GO
+ALTER TABLE oys.ChildPlate
+	ADD CONSTRAINT PK_ChildPlate PRIMARY KEY NONCLUSTERED (ProgramGUID,ChildPlateGUID);
+ALTER TABLE oys.ChildPlate
+	ADD CONSTRAINT FK_ChildPlate_Program FOREIGN KEY (ProgramGUID) 
+		REFERENCES oys.Program(ProgramGUID) ON DELETE CASCADE;
+ALTER TABLE oys.ChildPlate
+	ADD CONSTRAINT UQ_ChildPlate_ChildPlateGUID UNIQUE (ChildPlateGUID);
+CREATE UNIQUE CLUSTERED INDEX IX_AutoId ON oys.ChildPlate(AutoId);
+GO
+
+USE SNDBaseDev
+CREATE TABLE oys.ChildPart (
+    AutoId BIGINT IDENTITY(1,1) NOT NULL,
+    DBEntryDateTime DATETIME DEFAULT GETDATE(),
+    ChildPlateGUID UNIQUEIDENTIFIER NOT NULL,
+    ChildPartGUID UNIQUEIDENTIFIER NOT NULL,
+    ParentPartGUID UNIQUEIDENTIFIER NOT NULL,
+    SNPartName VARCHAR(100),
+    SAPPartName VARCHAR(100),
+    QtyProgram INT,
+    Job VARCHAR(50),
+    Shipment VARCHAR(50),
+    TrueArea FLOAT,
+    NestedArea FLOAT
+);
+GO
+ALTER TABLE oys.ChildPart
+	ADD CONSTRAINT PK_ChildPart PRIMARY KEY NONCLUSTERED (ChildPlateGUID,ChildPartGUID);
+ALTER TABLE oys.ChildPart
+	ADD CONSTRAINT FK_ChildPart_ChildPlate FOREIGN KEY (ChildPlateGUID) 
+		REFERENCES oys.ChildPlate(ChildPlateGUID) ON DELETE CASCADE;
+CREATE UNIQUE CLUSTERED INDEX IX_AutoId ON oys.ChildPart(AutoId);
+GO
+
+USE SNDBaseDev
+CREATE TABLE oys.Remnant (
+    AutoId BIGINT IDENTITY(1,1) NOT NULL,
+    DBEntryDateTime DATETIME DEFAULT GETDATE(),
+    ChildPlateGUID UNIQUEIDENTIFIER NOT NULL,
+    RemnantGUID UNIQUEIDENTIFIER NOT NULL,
+    RemnantName VARCHAR(50),
+    Area FLOAT,
+    IsRectangular BIT,
+    RectWidth FLOAT,
+    RectLength FLOAT
+);
+GO
+ALTER TABLE oys.Remnant
+	ADD CONSTRAINT PK_Remnant PRIMARY KEY NONCLUSTERED (ChildPlateGUID,RemnantGUID);
+ALTER TABLE oys.Remnant
+	ADD CONSTRAINT FK_Remnant_ChildPlate FOREIGN KEY (ChildPlateGUID) 
+		REFERENCES oys.ChildPlate(ChildPlateGUID) ON DELETE CASCADE;
+CREATE UNIQUE CLUSTERED INDEX IX_AutoId ON oys.Remnant(AutoId);
+GO
+
+USE SNDBaseDev
+CREATE TABLE oys.Status (
+    AutoId BIGINT IDENTITY(1,1) NOT NULL,
+    DBEntryDateTime DATETIME DEFAULT GETDATE(),
+    ProgramGUID UNIQUEIDENTIFIER NOT NULL,
+    StatusGUID UNIQUEIDENTIFIER NOT NULL,
+    SigmanestStatus VARCHAR(64), -- 'Created', 'Released', or 'Deleted'
+    SAPStatus VARCHAR(64)
+);
+GO
+ALTER TABLE oys.Status
+	ADD CONSTRAINT PK_Status PRIMARY KEY NONCLUSTERED (ProgramGUID,StatusGUID);
+ALTER TABLE oys.Status
+	ADD CONSTRAINT FK_Status_Program FOREIGN KEY (ProgramGUID) 
+		REFERENCES oys.Program(ProgramGUID) ON DELETE CASCADE;
+CREATE UNIQUE CLUSTERED INDEX IX_AutoId ON oys.Status(AutoId);
+GO
+
+USE SNDBaseDev
+CREATE TABLE oys.StatusArchive (
+    AutoId BIGINT NOT NULL,
+    DBEntryDateTime DATETIME DEFAULT GETDATE(),
+    ProgramGUID UNIQUEIDENTIFIER,
+    StatusGUID UNIQUEIDENTIFIER,
+    SigmanestStatus VARCHAR(64),
+    SAPStatus VARCHAR(64),
+    ArchiveDateTime DATETIME DEFAULT GETDATE()
+);
+GO
+
+
