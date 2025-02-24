7f592c0070e84672ed558ba35f4b1ab1faf05304
Patrick Miller <paddymills@proton.me>
2025-02-23 20:17:41 -0500

    add uninstall script

---
+++ schema/Uninstall.sql
@@ -0,0 +1,14 @@
+
+DROP PROCEDURE sap.PushSapDemand;
+DROP PROCEDURE sap.PushRenamedDemand;
+DROP PROCEDURE sap.RemoveRenamedDemand;
+DROP PROCEDURE sap.PushSapInventory;
+DROP PROCEDURE sap.GetFeedback;
+DROP PROCEDURE sap.MarkFeedbackSapUploadComplete;
+DROP PROCEDURE sap.UpdateProgram;
+
+DROP TABLE sap.InterfaceConfig;
+DROP TABLE sap.RenamedDemandAllocation;
+
+DROP SCHEMA sap;
+GO
