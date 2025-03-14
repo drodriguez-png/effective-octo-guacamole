bc673c40886e2787bdd8c13314596c1bb4844a98
Patrick Miller <pimiller@high.net>
2025-03-14 12:06:15 -0400

    process change removed todo

--- schema/SapInter_Proc.sql
+++ schema/SapInter_Proc.sql
@@ -73,8 +73,6 @@ GO
 -- ********************************************
 CREATE OR ALTER PROCEDURE sap.PushSapDemand
 	@sap_event_id VARCHAR(50) NULL,	-- SAP: numeric 20 positions, no decimal
-
-	-- TODO: impl SAP MM usage to escape truncation due to character limit
 	@sap_part_name VARCHAR(18),
 
 	@work_order VARCHAR(50),
