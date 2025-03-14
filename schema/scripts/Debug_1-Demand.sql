0a9e679b47018e7ad18811d94dd69dbd64285930
Patrick Miller <pimiller@high.net>
2025-03-14 16:10:20 -0400

    split debug scripts per interface

---
+++ schema/scripts/Debug_1-Demand.sql
@@ -0,0 +1,41 @@
+
+--delete from SNDBaseDev.dbo.TransAct
+--where TransType='SN60'
+--and ( Material is null or Material like '%NA%' );
+
+--delete from SNDBaseDev.dbo.TransAct;
+
+--select * from SNDBaseDev.dbo.TransAct;
+select
+	AutoInc,
+	TransType,
+	District,
+	TransID,
+	OrderNo as WorkOrder,
+	ItemName as PartName,
+	OnHold,		-- part is available for nesting
+	Qty,
+	Material,	-- {spec}-{grade}{test}
+	Customer,	-- State(occurrence)
+	DwgNumber,	-- Drawing name
+	Remark,		-- autoprocess instruction
+	ItemData1 as Job,	-- Job(project)
+	ItemData2 as Shipment,	-- Shipment
+	ItemData3 as RawMaterialMaster,
+	ItemData4 as Operation1,
+	ItemData5 as Operation2,
+	ItemData6 as Operation3,
+	ItemData9 as Mark,	-- part name (Material Master with job removed)
+	ItemData10 as HeatSwapKeyword,
+	ItemData16 as PartHoursOrder,
+	ItemData17 as SapPartName,
+	ItemData18 as SapEventId,
+	AddedDate
+from SNDBaseDev.dbo.TransAct
+where TransType like 'SN8%'
+;
+
+select *
+from log.SapDemandCalls
+where LogDate > CAST(CAST(GETDATE() AS DATE) AS DATETIME) + '14:00:00'
+;
